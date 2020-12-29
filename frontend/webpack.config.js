69b6231fc7423626f200d79c6200c4afa1c513c7
villem <maandi.villem@gmail.com>
2020-12-29 21:00:43 +0200

    initial commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const ForkTsCheckerWebpackPlugin = require( 'fork-ts-checker-webpack-plugin' );
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        chunkFilename: '[chunkhash].js',
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            favicon: './src/asset/favicon.ico',
+        }),
+    ],
+};
